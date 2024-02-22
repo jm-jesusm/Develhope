cbc417865334480ad739710af70e6c18da36ea1a
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-02-22 14:04:45 +0100

    Completed js es3

--- javascript/es3/exercise.js
+++ javascript/es3/exercise.js
@@ -1,19 +1,21 @@
-const sum = function (a, b) {
+const sum = (a, b) => {
   return a + b;
 }
 
-const subtract = function (a, b) {
+const subtract = (a, b) => {
   return a - b;
 }
 
-const multiply = function (a, b) {
+const multiply = (a, b) => {
   return a * b;
 }
 
-const divide = function (a, b) {
+const divide = (a, b) => {
   return a / b;
 }
 
-const log = function (value) {
+const log = (value) => {
   console.log(value);
 }
+
+log(divide(subtract(multiply(sum(2,4), sum(5,2)), 2), 5))
