b9313d80f52d01c33ad9421c6d46cc66ec3565c0
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-03-12 10:19:42 +0100

    Completed js es26

--- javascript/es26/exercise.js
+++ javascript/es26/exercise.js
@@ -1,5 +1,5 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+function sum(...params) {
+    return params.reduce((acc, curr) => acc+curr, 0)
 }
 
 console.log(sum(1, 2, 3, 4, 5));
